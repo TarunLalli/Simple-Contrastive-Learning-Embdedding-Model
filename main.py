b832d7ccd69d4be7e7a06b237390b7adb08522ed
TarunLalli <tarunslalli@outlook.com>
2026-01-02 12:16:08 +0000

    Adding basic structure

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import torch
 import torch.nn as nn
 
+class Encoder_f(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        ...
+
+class ProjectionHead_g(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        ...
+
+class ContrastiveModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        ...
