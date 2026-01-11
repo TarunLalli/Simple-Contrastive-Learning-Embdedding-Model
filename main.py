8eee7c61403a32a193d3bfa71f5b717f656e37e0
TarunLalli <tarunslalli@outlook.com>
2026-01-11 15:04:38 +0000

    Restructuring main.py to have all operations in one class

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 import torch
 import torch.nn as nn
-from data import training_corpus
 
-class Encoder_f(nn.Module):
-    def __init__(self,d_input,d_model, h):
+class Encoder(nn.Module):
+    def __init__(self, d_model, d_proj, vocab_size, pad_id):
         super().__init__()
-        self.ln1 = nn.Linear(d_input,d_model)
-        self.ln2 = nn.Linear(d_model, h)
-
-    def forward(self,x):
-        #Applying MLP
-        #Applying first FFLayer
-        output = self.ln1(x)
-        #ReLu
-        output = nn.ReLU(x)
-        #Applying second FFLayer
-        output = self.ln2(x)
-        #Returning model output
-        return output
-
-class ProjectionHead_g(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self):
-        ...
-
-class ContrastiveModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self):
-        ...
+        self.d_model = d_model
+        self.d_proj = d_proj
+        self.vocab_size = vocab_size
+        self.pad_id = pad_id
+        self.embed = nn.Embedding(vocab_size, d_model)
+        
+    def forward(self,input_ids):
+        # Creating mask
+        mask_bool = 
